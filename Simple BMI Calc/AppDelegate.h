9228716676de47ecfa1f804d3350e6db93e4c0a7
Ian Vergara <ian.vergara@gmail.com>
2013-02-14 21:33:53 -0800

    Initial Commit

---
+++ Simple BMI Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simple BMI Calc
+//
+//  Created by Ian Vergara on 2/14/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Simple BMI Calc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Simple BMI Calc
+//
+//  Created by Ian Vergara on 2/14/13.
+//  Copyright (c) 2013 Ian Vergara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
